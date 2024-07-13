a3720b20058c7c7fb8c01f108db5aa8120e92a0e
koog7 <k.ognev709@mail.com>
2024-07-13 12:55:49 +0600

    Создан компонент со страницей если пользователь перейдет по несуществующему пути

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import './App.css'
+import {NavLink, Route, Routes} from "react-router-dom";
+import Home from "./containers/Home.tsx";
+import AddForm from "./containers/AddForm.tsx";
+import NotFound from "./components/NotFound.tsx";
 
 const App = () => {
     return (
         <>
-
+            <div style={{backgroundColor: '#404040', width: '1000px', minHeight: '50px', padding: '2px'}}>
+                <h2 style={{marginLeft: '10px'}}><NavLink className="nav-link" to="/" style={{textDecoration: 'none', color: 'white'}}>ToDo list</NavLink></h2>
+            </div>
+            <hr/>
+            <Routes>
+                <Route path="/" element={(
+                    <Home/>
+                )}/>
+                <Route
+                    path="*"
+                    element={<NotFound />}
+                />
+            </Routes>
         </>
     )
 };
-
 
 
 export default App
---
+++ src/components/NotFound.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <div className="not-found-container">
+            <h1 className="not-found-header">404</h1>
+            <p className="not-found-text">Page Not Found</p>
+        </div>
+    );
+};
+
+export default NotFound;
